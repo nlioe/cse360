346d157b853a376400334ba41224b636ed1df4b8
novilia <nlioe@asu.edu>
2019-10-07 23:46:56 -0700

    Initial version

---
+++ Assignment/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,67 @@
+package cse360assign2;
+
+/**
+ * Author: Novilia Lioe
+ * Class ID: 70642, CSE360 W 9:40am 
+ * Assignment #: 2
+ * Description: This assignment is to practice with using a version control system.
+ *              Implement program that perform addition and subtraction, 
+ *              then display the total.
+ */
+
+public class AddingMachine {
+
+    private int total;
+
+    /**
+     * Class constructor.
+     */
+    public AddingMachine() {
+        total = 0; // not needed - included for clarity
+    }
+
+    /**
+     * Get the current total.
+     * 
+     * @return total The current total
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * Add the parameter to the total variable.
+     * 
+     * @param value The value to be added
+     */
+    public void add(int value) {
+
+    }
+
+    /**
+     * Subtract the parameter from the total variable.
+     * 
+     * @param value The value to be subtract
+     */
+    public void subtract(int value) {
+
+    }
+
+    /**
+     * Get string representation of the object.
+     * Record a history of the transactions that start from 
+     * the initial 0 value to the last input.
+     * 
+     * @return history A history of the transactions
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * Reset all of the elements to default.
+     */
+    public void clear() {
+
+    }
+}
